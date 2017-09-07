f8b8f8d78d1bc0785502f17da6fe65eae500d4f9
d.orteney <d.orteney@kdvm.ru>
2017-09-07 08:47:30 +0700

    added textboxes

--- app/src/main/java/com/example/testapplication/modules/main/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/modules/main/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.testapplication.modules.main
 
+import android.app.DownloadManager
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.testapplication.R
 import com.example.testapplication.modules.flexboxlist.FlexBoxListActivity
 import com.example.testapplication.modules.simplelist.SimpleListActivity
 import com.example.testapplication.modules.textarea.TextAreaActivity
+import com.example.testapplication.modules.textboxes.TextBoxesActivity
 import com.example.testapplication.modules.textviews.TextViewsActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.sdk19.listeners.onClick
@@ -25,5 +28,11 @@ class MainActivity : AppCompatActivity() {
         showFlexBoxListButton.onClick { startActivity<FlexBoxListActivity>() }
         showTextAreaButton.onClick { startActivity<TextAreaActivity>() }
         showTextViewsButton.onClick { startActivity<TextViewsActivity>() }
+        showTexBoxesButton.onClick { startActivity<TextBoxesActivity>() }
+
+        openFileDirectoryButton.onClick {
+            startActivity(Intent(DownloadManager.ACTION_VIEW_DOWNLOADS))
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/testapplication/modules/textboxes/TextBoxesActivity.kt
@@ -0,0 +1,61 @@
+package com.example.testapplication.modules.textboxes
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.example.testapplication.R
+import kotlinx.android.synthetic.main.activity_text_boxes.*
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.sdk19.listeners.onClick
+import org.jetbrains.anko.singleTop
+
+class TextBoxesActivity : AppCompatActivity() {
+
+    private var isLightTheme = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if(savedInstanceState == null) {
+            isLightTheme = intent.getBooleanExtra("isLightTheme", true)
+        }
+
+        setTheme(if (isLightTheme) R.style.AppTheme else R.style.DarkTheme)
+
+        setContentView(R.layout.activity_text_boxes)
+
+        initViews()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_theme, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_change_theme -> {
+                startActivity(intentFor<TextBoxesActivity>("isLightTheme" to !isLightTheme).singleTop())
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun initViews() {
+        showErrorButton.onClick { showErrors() }
+        hideErrorButton.onClick { hideErrors() }
+    }
+
+    private fun showErrors() {
+        firstTextBox.setError("Я у мамы ошибка!")
+        secondTextBox.setError("Я у мамы ошибка!")
+    }
+
+    private fun hideErrors() {
+        firstTextBox.removeError()
+        secondTextBox.removeError()
+    }
+}
