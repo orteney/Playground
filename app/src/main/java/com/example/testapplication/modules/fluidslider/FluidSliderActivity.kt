e48f776b61643ae8bd45c5ba61127710fe07012f
Domovoy19 <domovoy1905@gmail.com>
2018-02-16 19:02:34 +0300

    added fluid slider

---
+++ app/src/main/java/com/example/testapplication/modules/fluidslider/FluidSliderActivity.kt
@@ -0,0 +1,19 @@
+package com.example.testapplication.modules.fluidslider
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.testapplication.R
+
+class FluidSliderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fluid_slider)
+
+        initViews()
+    }
+
+    private fun initViews() {
+
+    }
+}
